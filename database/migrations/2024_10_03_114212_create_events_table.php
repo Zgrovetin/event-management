a1ae99c3c60788d6970397791075076a7fbfbe73
Zgrovetin <534477@gmail.com>
2024-10-03 18:27:00 +0200

    Custom seeder classes
    Generating data using factories and seeders

--- database/migrations/2024_10_03_114212_create_events_table.php
+++ database/migrations/2024_10_03_114212_create_events_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,8 +19,8 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
 
-            $table->dateTime('start_date');
-            $table->dateTime('end_date');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
 
             $table->timestamps();
         });

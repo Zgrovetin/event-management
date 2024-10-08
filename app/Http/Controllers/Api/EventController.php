ef431d68f7acdbe4514a395f965ccbe43a2095ab
Zgrovetin <534477@gmail.com>
2024-10-08 12:41:49 +0200

    Used in general permission checking
    Work with controllers & resources,
    things not tied with a specific db model nor resource model like

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
 use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
+//use Illuminate\Auth\Access\Gate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
 
 class EventController extends Controller
 {
@@ -72,6 +75,12 @@ class EventController extends Controller
      */
     public function update(Request $request, Event $event)
     {
+//        if (Gate::denies('update-event', $event)) {
+//            abort(403, 'You are not allowed to update event.');
+//        }
+//        The next line will do the same as previous three!
+        $this->authorize('update-event', $event);
+
         $event->update(
             $request->validate([
             'name' => 'sometimes|string|max:255',

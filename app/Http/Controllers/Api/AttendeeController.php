34b33ee9f165b7e23682ce6b7ad267826b08eedc
Zgrovetin <534477@gmail.com>
2024-10-09 19:44:28 +0200

    Consider using Redis

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -17,6 +17,7 @@ class AttendeeController extends Controller
 
     public function __construct() {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')->only(['store', 'destroy']);
         $this->authorizeResource(Attendee::class, 'attendee');
     }
 
@@ -38,7 +39,7 @@ class AttendeeController extends Controller
     {
         $attendee = $this->loadRelationships(
             $event->attendees()->create([
-                'user_id' => 1
+                'user_id' => $request->user()->id
             ])
         );
 
@@ -68,7 +69,6 @@ class AttendeeController extends Controller
      */
     public function destroy(Event $event, Attendee $attendee)
     {
-//        $this->authorize('delete-attendee', [$event, $attendee]);
         $attendee->delete();
 
         return response(status: 204);
